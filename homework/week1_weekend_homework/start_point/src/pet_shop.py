17add260c04ba99f3af697a34817d093f300921a
Campbell Murray <cam@campbells-air.home>
2020-09-06 01:17:08 +0100

    beep

--- homework/week1_weekend_homework/start_point/src/pet_shop.py
+++ homework/week1_weekend_homework/start_point/src/pet_shop.py
@@ -1,15 +1,42 @@
 # WRITE YOUR FUNCTIONS HERE
 
-def get_pet_shop_name(setup):
-    return setup["name"]
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
 
-def get_total_cash(setup):
-    return setup["admin"]["total_cash"]
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash(setup, amount):
-    cash_before = get_total_cash(setup)
-    total_cash = cash_before + amount
-    return total_cash
+def add_or_remove_cash(pet_shop, amount):
+     cash = get_total_cash(pet_shop)
+     new_cash = cash + amount
+     pet_shop["admin"]["total_cash"] = new_cash
+     return get_total_cash(pet_shop)
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, new_sales):
+    sold = get_pets_sold(pet_shop)
+    new_sold = sold + new_sales
+    pet_shop["admin"]["pets_sold"] = new_sold
+    return get_pets_sold(pet_shop)
+
+def get_stock_count(pet_shop):
+    count = len(pet_shop["pets"])
+    return count
+
+
+
+
+
+
+
+
+
+
+    
+    
+
 
 
 
--- homework/week1_weekend_homework/start_point/tests/pet_shop_test.py
+++ homework/week1_weekend_homework/start_point/tests/pet_shop_test.py
@@ -78,7 +78,7 @@ class TestPetShop(unittest.TestCase):
     def test_pet_shop_name(self):
         name = get_pet_shop_name(self.cc_pet_shop)
         self.assertEqual("Camelot of Pets", name)
-
+    
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
@@ -88,24 +88,20 @@ class TestPetShop(unittest.TestCase):
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1010, cash)
 
-    @unittest.skip("delete this line to run the test")
     def test_add_or_remove_cash__remove(self):
         add_or_remove_cash(self.cc_pet_shop,-10)
         cash = get_total_cash(self.cc_pet_shop)
         self.assertEqual(990, cash)
 
-    @unittest.skip("delete this line to run the test")
     def test_pets_sold(self):
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(0, sold)
 
-    @unittest.skip("delete this line to run the test")
     def test_increase_pets_sold(self):
         increase_pets_sold(self.cc_pet_shop,2)
         sold = get_pets_sold(self.cc_pet_shop)
         self.assertEqual(2, sold)
 
-    @unittest.skip("delete this line to run the test")
     def test_stock_count(self):
         count = get_stock_count(self.cc_pet_shop)
         self.assertEqual(6, count)
